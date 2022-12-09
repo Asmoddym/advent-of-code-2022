1340a725658521095ab8d7829b9798be2b1fd4d0
Sylvain CHAUGNY <sylvain.chaugny@elevo.io>
2022-12-09 14:20:50 +0100

    Day 09 part 1

---
+++ day_09/day_09.rb
@@ -0,0 +1,47 @@
+@data = File.read("input.txt").split("\n")
+
+@rope_position = [{ x: 0, y: 0 }, { x: 0, y: 0 }]
+@visited_tail_positions = []
+
+def must_move_tail?()
+  (@rope_position[0][:x] - @rope_position[1][:x]).abs > 1 ||
+    (@rope_position[0][:y] - @rope_position[1][:y]).abs > 1
+end
+
+def move_rope_of(x, y)
+  old_head_position = @rope_position[1].dup
+
+  @rope_position[1][:x] += y
+  @rope_position[1][:y] += x
+
+  @rope_position[0] = old_head_position if must_move_tail?()
+end
+
+def move_rope(direction, count)
+  count.times do |i|
+    case direction
+    when 'U'
+      move_rope_of(-1, 0)
+    when 'D'
+      move_rope_of(1, 0)
+    when 'L'
+      move_rope_of(0, -1)
+    when 'R'
+      move_rope_of(0, 1)
+    end
+
+    @visited_tail_positions << @rope_position[0].dup
+  end
+end
+
+def part_1
+  @data.each do |line|
+    direction, count = line.split(' ')
+
+    move_rope(direction, count.to_i)
+  end
+end
+
+part_1
+
+puts @visited_tail_positions.uniq.count
